9163c97faf09cc9c9ef62bc5ee2c207946e53ec0
Fernando Gabrieli <fernando.gabrieli@networksolutions.com>
2018-06-14 23:06:07 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'src'),
+        filename: 'webpack-bundle-[name].js'
+    },
+    devtool: 'src-map',
+    plugins: [
+        new BrowserSyncPlugin({
+            // browse to http://localhost:3000/ during development,
+            // ./public directory is being served
+            host: 'localhost',
+            port: 3000,
+            server: {baseDir: ['src']}
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: 'html-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader'
+            }, {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                loader: 'file-loader?name=src/fonts/[name].[ext]'
+            }
+        ]
+    }
+};
